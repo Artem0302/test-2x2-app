aaf3aad05e114a15ab8d7116c70a109156a89a2d
Artem Alieksieiev <artem.alieksieiev@gmail.com>
2024-05-17 14:17:52 +0300

    Added img validation

--- src/screens/info-screen/info-screen.tsx
+++ src/screens/info-screen/info-screen.tsx
@@ -1,6 +1,6 @@
 import {useRoute} from '@react-navigation/native';
 import {InfoHeader} from '@screens/info-screen/components';
-import React from 'react';
+import React, {useState} from 'react';
 import {Image, SafeAreaView, ScrollView, View} from 'react-native';
 import {COLORS} from '@shared/constants';
 import {TInfoScreenNavigatorType} from '@shared/types';
@@ -12,6 +12,7 @@ type TInfoScreenRouteProp = TInfoScreenNavigatorType['route'];
 export function InfoScreen() {
   const route = useRoute<TInfoScreenRouteProp>();
   const {item} = route.params;
+  const [source, setSource] = useState(item.img_url);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -19,20 +20,23 @@ export function InfoScreen() {
         <InfoHeader title={item.title} />
 
         <View>
-          <Image
-            defaultSource={{
-              uri: 'https://s3-alpha-sig.figma.com/img/75a4/05fa/f002f29b525831aebc083a3a3d76c40e?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JVpyn94f-wRzYxwErgVd6YMrK3ys-Cie95S7iNW8BOkjqNyEcdr3V0opHBgG9rJbTKx-d~Y0fiDf1M1JUxRZrfFGBeC0RSVxEzV3G-Ehv2UUJWBG1~BrwJUr3-vZ2CVXz6l-OJ9KjMOlICdy7HqAaTEogb4S2cUzycrTFCT1Z36xHTjzZZtS87VFhapCa9mRKTrPSFVTpNVSHM8yNgPEIcEnZlIRIs6Cy~OGl9nO-OobGdwSI1iCZWrZ3sH3YLU5SFQkev6pKzvpTyapQxk0uHtsrDcpue-QehXMKA2jJN4eKlUu-~blxo-Y29IyOgaC~4hVZ7CDQOH5~MS7WjHupA__',
-            }}
-            source={{
-              uri: item.img_url,
-            }}
-            style={styles.img}
-          />
+          {source && (
+            <Image
+              defaultSource={{
+                uri: 'https://s3-alpha-sig.figma.com/img/75a4/05fa/f002f29b525831aebc083a3a3d76c40e?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JVpyn94f-wRzYxwErgVd6YMrK3ys-Cie95S7iNW8BOkjqNyEcdr3V0opHBgG9rJbTKx-d~Y0fiDf1M1JUxRZrfFGBeC0RSVxEzV3G-Ehv2UUJWBG1~BrwJUr3-vZ2CVXz6l-OJ9KjMOlICdy7HqAaTEogb4S2cUzycrTFCT1Z36xHTjzZZtS87VFhapCa9mRKTrPSFVTpNVSHM8yNgPEIcEnZlIRIs6Cy~OGl9nO-OobGdwSI1iCZWrZ3sH3YLU5SFQkev6pKzvpTyapQxk0uHtsrDcpue-QehXMKA2jJN4eKlUu-~blxo-Y29IyOgaC~4hVZ7CDQOH5~MS7WjHupA__',
+              }}
+              source={{
+                uri: item.img_url,
+              }}
+              style={styles.img}
+              onError={() => setSource('')}
+            />
+          )}
 
           <View
             style={[
               styles.text_wrapper,
-              item.img_url ? styles.text_wrapper_alt : {},
+              source ? styles.text_wrapper_alt : {},
             ]}>
             <Text
               color={COLORS.text_title}
--- src/widgets/news-widget/news-widget.tsx
+++ src/widgets/news-widget/news-widget.tsx
@@ -1,5 +1,5 @@
 import {useNavigation} from '@react-navigation/native';
-import React, {memo} from 'react';
+import React, {memo, useState} from 'react';
 import {Image, TouchableOpacity, View} from 'react-native';
 import {COLORS} from '@shared/constants';
 import {INews, THomeScreenNavigatorType} from '@shared/types';
@@ -14,24 +14,23 @@ interface INewsWidget {
 }
 
 export const NewsWidget = memo(({onLongPress, item}: INewsWidget) => {
+  const [source, setSource] = useState(item.img_url);
   const navigation = useNavigation<THomeScreenNavProp>();
 
   const onPress = () => navigation.navigate('MAIN.INFO_SCREEN', {item});
 
   return (
     <TouchableOpacity
-      style={[styles.container, item.img_url ? {height: 340} : {}]}
+      style={[styles.container, source ? {height: 340} : {}]}
       onPress={onPress}
       onLongPress={() => onLongPress(item.id)}>
-      {item.img_url && (
+      {source && (
         <Image
           style={styles.img}
-          defaultSource={{
-            uri: 'https://s3-alpha-sig.figma.com/img/75a4/05fa/f002f29b525831aebc083a3a3d76c40e?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JVpyn94f-wRzYxwErgVd6YMrK3ys-Cie95S7iNW8BOkjqNyEcdr3V0opHBgG9rJbTKx-d~Y0fiDf1M1JUxRZrfFGBeC0RSVxEzV3G-Ehv2UUJWBG1~BrwJUr3-vZ2CVXz6l-OJ9KjMOlICdy7HqAaTEogb4S2cUzycrTFCT1Z36xHTjzZZtS87VFhapCa9mRKTrPSFVTpNVSHM8yNgPEIcEnZlIRIs6Cy~OGl9nO-OobGdwSI1iCZWrZ3sH3YLU5SFQkev6pKzvpTyapQxk0uHtsrDcpue-QehXMKA2jJN4eKlUu-~blxo-Y29IyOgaC~4hVZ7CDQOH5~MS7WjHupA__',
-          }}
           source={{
-            uri: item.img_url,
+            uri: source,
           }}
+          onError={() => setSource('')}
         />
       )}
 
